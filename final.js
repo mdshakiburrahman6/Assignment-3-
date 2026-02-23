9962a2b5f078a520d9942bfa180a3611ff6daa21
mdshakiburrahman6 <mdshakiburrahman6.com>
2026-02-24 00:04:55 +0600

    Assignment 3 - problem solving(Solved 2)

---
+++ final.js
@@ -0,0 +1,19 @@
+/** Problem -01 ( Divide the Asset ) */
+var area = 15;
+//write your code here
+var land = area / 2;
+console.log(land);
+
+
+/** Problem -02 ( Cycle or Laptop ) */
+var money = 9999;
+//write your code here
+if(money >= 25000){
+    console.log("Laptop");
+}else if(money >=10000 && money <=25000) {
+    console.log("Cycle");
+} else {
+    console.log("Chocolate");
+}
+
+
---
+++ problem1.js
@@ -0,0 +1,7 @@
+/** Problem -01 ( Divide the Asset ) */
+var area = 15;
+//write your code here
+var land = area / 2;
+
+console.log(land);
+
